15212bf2b87394137d65871d5a53ed5383aa0447
oluyidaniel <oluwatobidaniel090@gmail.com>
2026-02-15 23:03:02 +0100

    auht

--- frontend/script/signup.js
+++ frontend/script/signup.js
@@ -29,7 +29,7 @@ submitBtn.addEventListener("click", async () => {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/api/auth/signup", {
+    const response = await fetch("https://party-backend-mj21.onrender.com/api/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ firstName, lastName, email, password, confirmPassword })
